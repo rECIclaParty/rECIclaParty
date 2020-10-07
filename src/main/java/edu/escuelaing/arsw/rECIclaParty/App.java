6b642ee358b3b5f6ac39859a28860c8c2443bb0d
Juan Garcia <juan.garcia-ga@mail.escuelaing.edu.co>
2020-10-07 02:16:21 -0500

    solucionando error despliegue

--- src/main/java/edu/escuelaing/arsw/rECIclaParty/App.java
+++ src/main/java/edu/escuelaing/arsw/rECIclaParty/App.java
@@ -4,32 +4,25 @@ import edu.escuelaing.arsw.rECIclaParty.models.services.UserRepository;
 import edu.escuelaing.arsw.rECIclaParty.models.users.Genero;
 import edu.escuelaing.arsw.rECIclaParty.models.users.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 
-import java.util.Date;
 
 @SpringBootApplication
-public class App 
-{
+@EnableAutoConfiguration
+public class App {
     @Autowired
-    private UserRepository users;
-    public static void main( String[] args )
-    {
+    private static UserRepository users;
 
-        SpringApplication app = new SpringApplication(App.class);
-        app.setBannerMode(Banner.Mode.OFF);
-        app.run(args);
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
 
-    }
-
-    public void run(String[] args){
         users.save(new Usuario("Jota Mario","jm@f.com", Genero.HOMBRE,"311","10-10-1999"));
         System.out.println(users.getByCorreo("jm@f.com"));
         users.findAll().forEach(x -> System.out.println(x));
-
-
     }
+
 }
+
